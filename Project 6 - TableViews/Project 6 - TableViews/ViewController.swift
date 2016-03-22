76d24e57e7b2a3e353d60e1b0909a86f82105691
Andrew Garcia <drewgarcia23@gmail.com>
2016-03-22 17:52:20 -0500

    Added Simple TableView

---
+++ Project 6 - TableViews/Project 6 - TableViews/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project 6 - TableViews
+//
+//  Created by Andrew Garcia on 3/22/16.
+//  Copyright © 2016 Andrew Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var myExampleData = ["Alternative", "Rock", "Jazz"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel?.text = myExampleData[indexPath.row]
+        return cell
+    }
+
+
+}
+
