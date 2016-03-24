26030a89b5a2b2d9c011c227e4fd5efac6c4c97e
Andrew Garcia <drewgarcia23@gmail.com>
2016-03-24 18:39:14 -0500

    Added Project 12 - Swipe to Dismiss Keyboard

---
+++ Project 12 - SwipeKeyboard/Project 12 - SwipeKeyboard/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project 12 - SwipeKeyboard
+//
+//  Created by Andrew Garcia on 3/24/16.
+//  Copyright © 2016 Andrew Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            if swipeGesture.direction == UISwipeGestureRecognizerDirection.Down {
+                print("Swiped down, lets dismiss they keyboard.")
+                textField.resignFirstResponder()
+            }
+        }
+    }
+
+
+}
+
